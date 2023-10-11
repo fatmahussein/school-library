3b485f126b6c4d2415a0b6bd161146371da7015f
Fatma Hussein <fartunhussein346@gmail.com>
2023-10-11 12:10:10 +0300

    Modify constructor

--- student.rb
+++ student.rb
@@ -1,10 +1,10 @@
 require './person'
 
 class Student < Person
-  def initialize(classroom)
-    super(name, age)
-    @classroom = classroom
-  end
+    def initialize(classroom, age:, name: 'Unknown', parent_permission: true)
+        super(age: age, name: name, parent_permission: parent_permission)
+        @classroom = classroom
+     end
 
   def play_hooky
     '¯\\(ツ)/¯'
--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require './person'
 
 class Teacher < Person
-  def initialize(specialization)
-    super(name)
+    def initialize(specialization, age:, name: 'Unknown')
+        super(age: age, name: name)
     @specialization = specialization
   end
 
